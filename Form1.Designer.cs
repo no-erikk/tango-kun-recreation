1c96d09ea6ccbd3b38b8e6f000bca9083cfd9389
no-erikk <erik.youngquist@outlook.com>
2024-04-04 16:01:20 +0900

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace button_practice
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            submit = new Button();
+            cancel = new Button();
+            answerBox = new TextBox();
+            totalLabel = new Label();
+            correctLabel = new Label();
+            correctPercentageLabel = new Label();
+            yesOrNo = new Label();
+            questionBox = new Label();
+            totalData = new Label();
+            correctData = new Label();
+            correctPercentData = new Label();
+            SuspendLayout();
+            // 
+            // submit
+            // 
+            submit.Location = new Point(312, 229);
+            submit.Name = "submit";
+            submit.Size = new Size(112, 34);
+            submit.TabIndex = 0;
+            submit.Text = "回答";
+            submit.UseVisualStyleBackColor = true;
+            submit.Click += button1_Click;
+            // 
+            // cancel
+            // 
+            cancel.Location = new Point(447, 229);
+            cancel.Name = "cancel";
+            cancel.Size = new Size(112, 34);
+            cancel.TabIndex = 1;
+            cancel.Text = "リセット";
+            cancel.UseVisualStyleBackColor = true;
+            cancel.Click += button2_Click;
+            // 
+            // answerBox
+            // 
+            answerBox.Location = new Point(246, 170);
+            answerBox.Name = "answerBox";
+            answerBox.Size = new Size(390, 31);
+            answerBox.TabIndex = 2;
+            // 
+            // totalLabel
+            // 
+            totalLabel.AutoSize = true;
+            totalLabel.Location = new Point(57, 92);
+            totalLabel.Name = "totalLabel";
+            totalLabel.Size = new Size(88, 25);
+            totalLabel.TabIndex = 3;
+            totalLabel.Text = "表示回数";
+            totalLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // correctLabel
+            // 
+            correctLabel.AutoSize = true;
+            correctLabel.Location = new Point(57, 170);
+            correctLabel.Name = "correctLabel";
+            correctLabel.Size = new Size(88, 25);
+            correctLabel.TabIndex = 4;
+            correctLabel.Text = "正解回数";
+            correctLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // correctPercentageLabel
+            // 
+            correctPercentageLabel.AutoSize = true;
+            correctPercentageLabel.Location = new Point(57, 251);
+            correctPercentageLabel.Name = "correctPercentageLabel";
+            correctPercentageLabel.Size = new Size(88, 25);
+            correctPercentageLabel.TabIndex = 5;
+            correctPercentageLabel.Text = "正解確率";
+            correctPercentageLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // yesOrNo
+            // 
+            yesOrNo.AutoSize = true;
+            yesOrNo.Location = new Point(642, 173);
+            yesOrNo.Name = "yesOrNo";
+            yesOrNo.Size = new Size(0, 25);
+            yesOrNo.TabIndex = 6;
+            // 
+            // questionBox
+            // 
+            questionBox.AutoSize = true;
+            questionBox.Location = new Point(246, 125);
+            questionBox.Name = "questionBox";
+            questionBox.Size = new Size(59, 25);
+            questionBox.TabIndex = 7;
+            questionBox.Text = "label1";
+            // 
+            // totalData
+            // 
+            totalData.AutoSize = true;
+            totalData.Location = new Point(70, 117);
+            totalData.Name = "totalData";
+            totalData.Size = new Size(0, 25);
+            totalData.TabIndex = 8;
+            // 
+            // correctData
+            // 
+            correctData.AutoSize = true;
+            correctData.Location = new Point(70, 195);
+            correctData.Name = "correctData";
+            correctData.Size = new Size(0, 25);
+            correctData.TabIndex = 9;
+            // 
+            // correctPercentData
+            // 
+            correctPercentData.AutoSize = true;
+            correctPercentData.Location = new Point(70, 276);
+            correctPercentData.Name = "correctPercentData";
+            correctPercentData.Size = new Size(0, 25);
+            correctPercentData.TabIndex = 10;
+            // 
+            // mainWindow
+            // 
+            AcceptButton = submit;
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(813, 389);
+            Controls.Add(correctPercentData);
+            Controls.Add(correctData);
+            Controls.Add(totalData);
+            Controls.Add(questionBox);
+            Controls.Add(yesOrNo);
+            Controls.Add(correctPercentageLabel);
+            Controls.Add(correctLabel);
+            Controls.Add(totalLabel);
+            Controls.Add(answerBox);
+            Controls.Add(cancel);
+            Controls.Add(submit);
+            Name = "mainWindow";
+            Text = "単語くん";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button submit;
+        private Button cancel;
+        private TextBox answerBox;
+        private Label totalLabel;
+        private Label correctLabel;
+        private Label correctPercentageLabel;
+        private Label yesOrNo;
+        private Label questionBox;
+        private Label totalData;
+        private Label correctData;
+        private Label correctPercentData;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿namespace button_practice
+{
+    public partial class mainWindow : Form
+    {
+        // initialize counters
+        // カウンターの初期化
+        private int totalQuestions = 0;
+        private int correctQuestions = 0;
+
+        // helper func - add 1 to totalQuestions and refresh UI
+        // ヘルパー関数 - totalQuestionsに1を追加し、UIをリフレッシュする
+        private void updateTotalQues()
+        {
+            totalQuestions++;
+            totalData.Text = totalQuestions.ToString();
+        }
+
+        // helper func - add 1 to correctQuestions and refresh UI
+        // ヘルパー関数 - correctQuestionsに1を追加し、UIをリフレッシュする
+        private void updateCorrectAns()
+        {
+            correctQuestions++;
+            correctData.Text = correctQuestions.ToString(); 
+        }
+
+        // helper func - calculate percentCorrect and refresh UI
+        // ヘルパー関数 - パーセントコレクトを計算し、UIをリフレッシュする。
+        private void updatePercentCorrectAns()
+        {
+            double percentCorrect = (double)correctQuestions / totalQuestions * 100;
+            correctPercentData.Text = percentCorrect + "%";
+        }
+
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        // submit answer button
+        //「回答」を押すと
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // test strings - will replace with index numbers when database is iplemented
+            // テスト文字列 - データベースの実装時にインデックス番号に置き換える
+            string question = questionBox.Text.ToUpper();
+            string answer = answerBox.Text.ToUpper();
+
+            // check if answer is not empty
+            // 回答が空でないかチェックする
+            if (answer.Length > 0 )
+            {
+                if (question == answer)
+                {
+                    // if correct
+                    // 正解の場合
+                    yesOrNo.Text = "〇";
+                    updateCorrectAns();
+                }
+                else
+                {
+                    // if incorrect
+                    //　不正解の場合
+                    yesOrNo.Text = "×";
+                }
+
+                // run helper functions to update total questions and percentage of correct answers
+                //　ヘルパー関数を実行して、問題の合計と正解率を更新する
+                updateTotalQues();
+                updatePercentCorrectAns();
+
+                // clear answer box
+                // アンサーボックスを消去
+                answerBox.Text = string.Empty;
+
+            } else 
+            {
+                // if answer is empty show message box
+                // 答えが空ならメッセージボックスを表示
+                MessageBox.Show("答えを入力してください。");
+            }
+
+            
+
+        }
+        // reset button
+        //「リセット」を押すと
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // clear answer box and answer indicator
+            // アンサーボックスと〇×を消去
+            answerBox.Text = string.Empty;
+            yesOrNo.Text = string.Empty;
+        }
+
+    }
+}
